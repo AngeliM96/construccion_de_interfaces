45a0bdfcf0f3b0f9bf4e8286c50a7f2d155226d0
AngeliM96 <matiasangeli@gmail.com>
2021-06-04 23:13:32 -0300

    Faltaban comillas en resaltador

--- clase_4/resaltador.js
+++ clase_4/resaltador.js
@@ -1,4 +1,7 @@
 var parrafos = document.querySelectorAll("p");
 for (var i=0; i<parrafos.length; i++) {
-    parrafos[i].addEventListener("click", function() {this.classList.toggle(resaltar)});
+    parrafos[i].addEventListener("click", function() {
+            this.classList.toggle("resaltar")
+        }
+    );
 }
